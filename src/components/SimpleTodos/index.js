9fffc866aa885f7d96bb3dfef0f314cfae901d48
jayakumarkcomp <jayakumarkcomp@gamil.com>
2023-10-20 08:32:43 +0000

    first commit

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,4 +1,5 @@
 import {Component} from 'react'
+import {v4 as uuidV4} from 'uuid'
 
 import TodoItem from '../TodoItem'
 
@@ -38,49 +39,78 @@ const initialTodosList = [
     title: 'Get essentials for Sunday car wash',
   },
 ]
-class SimpleTodos extends Component{
-state = {todoList : initialTodosList}
 
-deleteTodo = id => {
-  const{todoList} = this.state
-  const updateTodoList = todoList.filter(eachTodo =>eachTodo.id !== id )
-  this.setState ({todoList:updateTodoList })
+class SimpleTodos extends Component {
+  state = {
+    todosList: initialTodosList,
+    titleInput: '',
+  }
 
+  renderTodoInputField = () => {
+    const {titleInput} = this.state
+
+    const onChangeHandler = event => {
+      this.setState({titleInput: event.target.value})
+    }
+
+    const onAddTodo = () => {
+      if (titleInput === '') return
+
+      this.setState(prevState => ({
+        todosList: [...prevState.todosList, {id: uuidV4(), title: titleInput}],
+        titleInput: '',
+      }))
+    }
+
+    return (
+      <div className="title-input-container">
+        <input value={titleInput} onChange={onChangeHandler} />
+        <button className="add-btn" type="button" onClick={onAddTodo}>
+          Add
+        </button>
+      </div>
+    )
+  }
+
+  deleteTodo = id => {
+    const {todosList} = this.state
+    const updatedTodosList = todosList.filter(eachTodo => eachTodo.id !== id)
+
+    this.setState({
+      todosList: updatedTodosList,
+    })
+  }
+
+  saveTodo = task => {
+    this.setState(prevState => ({
+      todosList: prevState.todosList.map(item =>
+        item.id === task.id ? task : item,
+      ),
+    }))
+  }
+
+  render() {
+    const {todosList} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="simple-todos-container">
+          <h1 className="heading">Simple Todos</h1>
+          {this.renderTodoInputField()}
+          <ul className="todos-list">
+            {todosList.map(eachTodo => (
+              <TodoItem
+                key={eachTodo.id}
+                todoDetails={eachTodo}
+                deleteTodo={this.deleteTodo}
+                saveTodo={this.saveTodo}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
-render(){
-  const{todoList}=this.state
-  return(
-    <div className = "app-container">
-    <div className = "simple-too-container">
-    <h1 className="heading"> Simple Todos</h1>
-    <ul className="todos-list">
-    {todoList.map(eachTodo=>(
-      <TodoItem
-    key = {eachTodo.id}
-    todoDetails ={eachTodo}
-    deleteTodo = {this.deleteTodo}
-    />
-    ))}
-    </ul>
-    </div>
-    </div>
-  )
-}
-}
+
 export default SimpleTodos
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
